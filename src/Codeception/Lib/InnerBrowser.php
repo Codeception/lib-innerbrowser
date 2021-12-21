b5f9c8cfb6109493dc7076deed6053c9a5f1b3e0
TavoNiievez <ganieves@outlook.com>
2021-12-20 20:26:01 -0500

    Update dependencies

--- src/Codeception/Lib/InnerBrowser.php
+++ src/Codeception/Lib/InnerBrowser.php
@@ -1013,7 +1013,7 @@ class InnerBrowser extends Module implements Web, PageSourceSaver, ElementLocato
         $formId = $form->attr('id');
         if ($formId !== null) {
             $fakeForm = $fakeDom->firstChild;
-            $topParent = $form->parents()->last();
+            $topParent = $form->ancestors()->last();
             $fieldsByFormAttribute = $topParent->filter(
                 sprintf('input[form=%s],select[form=%s],textarea[form=%s]', $formId, $formId, $formId)
             );
@@ -1044,7 +1044,7 @@ class InnerBrowser extends Module implements Web, PageSourceSaver, ElementLocato
         if (strcasecmp($node->first()->getNode(0)->tagName, 'form') === 0) {
             $form = $node->first();
         } else {
-            $form = $node->parents()->filter('form')->first();
+            $form = $node->ancestors()->filter('form')->first();
         }
 
         if (!$form) {
